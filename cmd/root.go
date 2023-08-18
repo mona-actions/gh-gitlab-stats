dab7e120d008bd4ca5633cf78fb7c955f0cc0ccf
Alejandro Menocal <amenocal@github.com>
2023-08-18 14:41:31 -0500

    refactor and init write to CSV

--- cmd/root.go
+++ cmd/root.go
@@ -4,11 +4,49 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"encoding/csv"
 	"log"
 	"os"
+	"strconv"
+	"time"
 
+	"github.com/pterm/pterm"
 	"github.com/spf13/cobra"
 	"github.com/xanzy/go-gitlab"
+)
+
+type GitLabSummary []ProjectSummary
+
+type ProjectSummary struct {
+	Namespace               string
+	ProjectName             string
+	IsEmpty                 bool
+	Last_Push               *time.Time
+	Last_Update             *time.Time
+	IsFork                  bool
+	RepoSize                int64
+	RecordCount             int
+	CollaboratorCount       int
+	ProtectedBranchCount    int
+	MergeRequestReviewCount int
+	MilestoneCount          int
+	IssueCount              int
+	MergeRequestCount       int
+	MRReviewCommentCount    int
+	CommitCommentCount      int
+	IssueCommentCount       int
+	IssueEventCount         int
+	ReleaseCount            int
+	BranchCount             int
+	TagCount                int
+	DiscussionCount         int
+	HasWiki                 bool
+	FullUrl                 string
+}
+
+var (
+	projectsSummary       [][]string
+	gitlabProjectsSummary []*ProjectSummary
 )
 
 // rootCmd represents the base command when called without any subcommands
@@ -19,17 +57,7 @@ var rootCmd = &cobra.Command{
 	      repositories, issues...`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	Run: func(cmd *cobra.Command, args []string) {
-		gitlabHostname := cmd.Flag("gitlab-hostname").Value.String()
-		gitlabToken := cmd.Flag("token").Value.String()
-		client := initClient(gitlabHostname, gitlabToken)
-		//getNamespaces(client)
-		getGroups(client)
-		gitlabProjects := getProjects(client)
-		getCommitActivity(gitlabProjects, client)
-		getMergeRequests(gitlabProjects, client)
-
-	},
+	Run: getGitlabStats,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -56,6 +84,60 @@ func init() {
 
 	rootCmd.Flags().StringP("token", "t", "", "The token to use to authenticate to the GitLab instance")
 	rootCmd.MarkFlagRequired("token")
+}
+
+func getGitlabStats(cmd *cobra.Command, args []string) {
+	gitlabHostname := cmd.Flag("gitlab-hostname").Value.String()
+	gitlabToken := cmd.Flag("token").Value.String()
+	client := initClient(gitlabHostname, gitlabToken)
+	//getNamespaces(client)
+	groupSpinnerSuccess, _ := pterm.DefaultSpinner.Start("Fetching Groups")
+	getGroups(client)
+	groupSpinnerSuccess.Success("Groups fetched successfully")
+
+	projectSpinnerSuccess, _ := pterm.DefaultSpinner.Start("Fetching Projects")
+	gitlabProjects := getProjects(client)
+	projectSpinnerSuccess.Success("Projects fetched successfully")
+
+	for _, project := range gitlabProjects {
+		log.Println("Found project", project.Name)
+		commitSpinnerSuccess, _ := pterm.DefaultSpinner.Start("Fetching Commits")
+		commits := getCommitActivity(project, client)
+		commitSpinnerSuccess.Success("Commits fetched successfully")
+
+		mergeRequestSpinnerSuccess, _ := pterm.DefaultSpinner.Start("Fetching Merge Requests")
+		mergeRequests := getMergeRequests(project, client)
+		mergeRequestSpinnerSuccess.Success("Merge Requests fetched successfully")
+
+		row := &ProjectSummary{
+			Namespace:   project.Namespace.Name,
+			ProjectName: project.Name,
+			IsEmpty:     project.EmptyRepo,
+			Last_Update: project.LastActivityAt,
+			RepoSize:    project.Statistics.RepositorySize,
+			IsFork:      project.ForkedFromProject != nil,
+			//CollaboratorCount:
+			//ProtectedBranchCount:
+			//MergeRequestReviewCount:
+			//MilestoneCount:
+			//IssueCount:
+			MergeRequestCount: len(mergeRequests),
+			//MRReviewCommentCount:
+			CommitCommentCount: len(commits),
+			//IssueCommentCount:
+			//IssueEventCount:
+			//ReleaseCount:
+			//BranchCount:
+			TagCount: len(project.TagList),
+			//DiscussionCount:
+			HasWiki: project.WikiEnabled,
+			FullUrl: project.WebURL,
+		}
+		gitlabProjectsSummary = append(gitlabProjectsSummary, row)
+	}
+
+	projectsSummary = convertToCSVFormat(gitlabProjectsSummary)
+	createCSV(projectsSummary, "projects.csv")
 }
 
 func initClient(hostname string, token string) *gitlab.Client {
@@ -112,6 +194,7 @@ func getProjects(client *gitlab.Client) []*gitlab.Project {
 			PerPage: 100,
 			Page:    1,
 		},
+		Statistics: gitlab.Bool(true),
 	}
 
 	for {
@@ -129,14 +212,10 @@ func getProjects(client *gitlab.Client) []*gitlab.Project {
 		opt.Page = response.NextPage
 	}
 
-	for _, project := range projects {
-		log.Println("Found project", project.Name)
-	}
-
 	return projects
 }
 
-func getCommitActivity(projects []*gitlab.Project, client *gitlab.Client) []*gitlab.Commit {
+func getCommitActivity(project *gitlab.Project, client *gitlab.Client) []*gitlab.Commit {
 	var commits []*gitlab.Commit
 	opt := &gitlab.ListCommitsOptions{
 		ListOptions: gitlab.ListOptions{
@@ -144,33 +223,28 @@ func getCommitActivity(projects []*gitlab.Project, client *gitlab.Client) []*git
 			Page:    1,
 		},
 	}
-	for _, project := range projects {
-		if project.EmptyRepo || project.RepositoryAccessLevel == "disabled" {
-			continue
+	for {
+		c, response, err := client.Commits.ListCommits(project.ID, opt)
+		if err != nil {
+			log.Fatalf("Failed to list commits: %v %v", response, err)
 		}
-		for {
-			c, response, err := client.Commits.ListCommits(project.ID, opt)
-			if err != nil {
-				log.Fatalf("Failed to list commits: %v %v", response, err)
-			}
-			commits = append(commits, c...)
+		commits = append(commits, c...)
 
-			if response.NextPage == 0 {
-				break
-			}
-
-			opt.Page = response.NextPage
+		if response.NextPage == 0 {
+			break
 		}
 
-		//TODO: Need to decide if we would like to build a new struct for more readable Commit Summary or just use the gitlab.Commit struct
-		for _, commit := range commits {
-			log.Println("Found commit", commit.ID, commit.ProjectID, commit.Title)
-		}
+		opt.Page = response.NextPage
+	}
+
+	//TODO: Need to decide if we would like to build a new struct for more readable Commit Summary or just use the gitlab.Commit struct
+	for _, commit := range commits {
+		log.Println("Found commit", commit.ID, commit.ProjectID, commit.Title)
 	}
 	return commits
 }
 
-func getMergeRequests(projects []*gitlab.Project, client *gitlab.Client) []*gitlab.MergeRequest {
+func getMergeRequests(project *gitlab.Project, client *gitlab.Client) []*gitlab.MergeRequest {
 	var mergeRequests []*gitlab.MergeRequest
 	opt := &gitlab.ListProjectMergeRequestsOptions{
 		ListOptions: gitlab.ListOptions{
@@ -178,29 +252,69 @@ func getMergeRequests(projects []*gitlab.Project, client *gitlab.Client) []*gitl
 			Page:    1,
 		},
 	}
-	for _, project := range projects {
-		if project.EmptyRepo || project.RepositoryAccessLevel == "disabled" {
-			continue
+
+	for {
+		p, response, err := client.MergeRequests.ListProjectMergeRequests(project.ID, opt)
+		if err != nil {
+			log.Fatalf("Failed to list merge requests: %v %v", response, err)
 		}
-		for {
-			p, response, err := client.MergeRequests.ListProjectMergeRequests(project.ID, opt)
-			if err != nil {
-				log.Fatalf("Failed to list merge requests: %v %v", response, err)
-			}
-			mergeRequests = append(mergeRequests, p...)
+		mergeRequests = append(mergeRequests, p...)
 
-			if response.NextPage == 0 {
-				break
-			}
-
-			opt.Page = response.NextPage
+		if response.NextPage == 0 {
+			break
 		}
 
-		for _, mergeRequest := range mergeRequests {
-			log.Println("Found merge request: ", mergeRequest.ID, mergeRequest.Title, mergeRequest.Author.Username)
-		}
+		opt.Page = response.NextPage
 	}
+
+	for _, mergeRequest := range mergeRequests {
+		log.Println("Found merge request: ", mergeRequest.ID, mergeRequest.Title, mergeRequest.Author.Username)
+	}
+
 	return mergeRequests
+}
+
+func createCSV(data [][]string, filename string) {
+	// Create team membership csv
+	file, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	// Initialize csv writer
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	// Write team memberships to csv
+
+	for _, line := range data {
+		writer.Write(line)
+	}
+}
+
+func convertToCSVFormat(projects []*ProjectSummary) [][]string {
+	var rows [][]string
+
+	// Add header row
+	header := []string{"Namespace Name", "Name", "Empty Repo", "Last Activity At", "Repository Size", "Commit Count"}
+	rows = append(rows, header)
+
+	// Add project rows
+	for _, project := range projects {
+		row := []string{
+			project.Namespace,
+			project.ProjectName,
+			strconv.FormatBool(project.IsEmpty),
+			project.Last_Update.Format(time.RFC3339),
+			strconv.FormatInt(project.RepoSize, 10),
+			strconv.Itoa(project.CommitCommentCount),
+			strconv.Itoa(project.MergeRequestCount),
+		}
+		rows = append(rows, row)
+	}
+
+	return rows
 }
 
 // Namespace will return all users and groups together
--- go.mod
+++ go.mod
@@ -3,19 +3,32 @@ module github.com/mona-actions/gh-gitlab-stats
 go 1.19
 
 require (
+	github.com/pterm/pterm v0.12.65
 	github.com/spf13/cobra v1.7.0
 	github.com/xanzy/go-gitlab v0.90.0
 )
 
 require (
+	atomicgo.dev/cursor v0.2.0 // indirect
+	atomicgo.dev/keyboard v0.2.9 // indirect
+	atomicgo.dev/schedule v0.0.2 // indirect
+	github.com/containerd/console v1.0.3 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/gookit/color v1.5.3 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/go-retryablehttp v0.7.2 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/lithammer/fuzzysearch v1.1.8 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	golang.org/x/net v0.8.0 // indirect
 	golang.org/x/oauth2 v0.6.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/term v0.10.0 // indirect
+	golang.org/x/text v0.11.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.29.1 // indirect
